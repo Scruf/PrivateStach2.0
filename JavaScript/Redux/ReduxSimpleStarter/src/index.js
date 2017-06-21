279404361dbd2bf135a73ab63e375841eb9995dc
Scruf <ek5442@g.rit.edu>
2017-06-20 21:45:56 -0400

    Index Fixed

--- JavaScript/Redux/ReduxSimpleStarter/src/index.js
+++ JavaScript/Redux/ReduxSimpleStarter/src/index.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 //Creating new component and this component will produce some html
 const App = function(){
-
   return <div> Hi </div>;
-
 }
+
 //Take this component and put it on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
