131eadd06057a4999ff48bab876af3b224031b0b
Scruf <ek5442@g.rit.edu>
2017-06-21 23:05:19 -0400

    Added event handlers

--- JavaScript/Redux/ReduxSimpleStarter/src/index.js
+++ JavaScript/Redux/ReduxSimpleStarter/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
+const API_KEY = 'AIzaSyB1Pf2gYt9IdprDA9cLWx3rDuhkxheJAe8'
 //Creating new component and this component will produce some html
-const App = function(){
-  return <div> Hi </div>;
+const App = () => {
+  return (<div> <SearchBar />
+  		 </div>)
 }
 
 //Take this component and put it on the page
