02abfb348d4a72927f554fe9cef872cb53fa01c8
Scruf <ek5442@g.rit.edu>
2017-06-22 21:38:07 -0400

    Something

--- JavaScript/Redux/ReduxSimpleStarter/src/components/search_bar.js
+++ JavaScript/Redux/ReduxSimpleStarter/src/components/search_bar.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react'
 class SearchBar extends Component{
 	
 	render(){
-		return <input onChange={this.onInputChange}/>
+		return <input onChange={event=>{console.log(event.target.value)}}/>
 	}
 
 	onInputChange(event){
