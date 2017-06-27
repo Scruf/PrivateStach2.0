c419b27e2339e521c09c79af505fc9f1c7bed6cd
Scruf <ek5442@g.rit.edu>
2017-06-26 21:11:14 -0400

    Added state

--- JavaScript/Redux/ReduxSimpleStarter/src/components/search_bar.js
+++ JavaScript/Redux/ReduxSimpleStarter/src/components/search_bar.js
@@ -2,15 +2,24 @@ import React, {Component} from 'react'
 
 
 class SearchBar extends Component{
+	constructor(props){
+		super(props);
+		this.state = {term:''};
+	}
 	
 	render(){
-		return <input onChange={event=>{console.log(event.target.value)}}/>
-	}
-
-	onInputChange(event){
-		console.log(event.target.value)
+		return(
+			<div>
+				<input 
+					value = {this.state.term}
+					onChange={event=>this.setState({term:event.target.value})} 
+				/>  
+	       </div>
+		);
 	}
 }
+	
 
 
 export default SearchBar;
+
--- JavaScript/Redux/ReduxSimpleStarter/src/index.js
+++ JavaScript/Redux/ReduxSimpleStarter/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search'
 const API_KEY = 'AIzaSyB1Pf2gYt9IdprDA9cLWx3rDuhkxheJAe8'
+
 //Creating new component and this component will produce some html
-const App = () => {
-  return (<div> <SearchBar />
-  		 </div>)
+class App extends Component{
+  constructor(props){
+  	super(props);
+  	this.state  = {videos:[]}
+  	
+	YTSearch({key:API_KEY,term:'Cats'},(videos)=>{
+		this.setState({videos})
+	})
+  }
+
+  render(){
+	  return (<div> <SearchBar />
+	  				<VideoList videos={this.state.videos}/>
+			 </div>)
+  	
+  }
 }
+
 
 //Take this component and put it on the page
 ReactDOM.render(<App />, document.querySelector('.container'));
