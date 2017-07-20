70397a421923821e6433015f6badfa7f60ba00ce
Scruf <ek5442@g.rit.edu>
2017-07-20 19:33:28 -0400

    Added blog

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,29 +1,41 @@
 class ArticlesController < ApplicationController
-	def index
-		@articles = Article.all
-	end
-
-	def new
-	end
-
-	def create
-		@article = Article.new(article_params)
-		if @article.save
-			redirect_to @article
-		else
-			render 'new'
-		end
-		#puts @article
-		# render plain: params[:article].inspect
-	end
-
-	def show
-		puts params
-		@article = Article.find(params[:id])
-	end
-
-	private
-		def article_params
-			params.require(:article).permit(:title, :Text)
-		end
+def index
+	@articles = Article.all
 end
+def new
+    @article = Article.new
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+def show
+    @article = Article.find(params[:id])
+end
+
+def create
+    @article = Article.new(article_params)
+    if @article.save
+        redirect_to @article
+    else
+        render 'new'
+    end
+end
+
+private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+end
+
+
